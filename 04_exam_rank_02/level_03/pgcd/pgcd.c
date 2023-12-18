398af8fbcb2ef3a27e63a583dd430ea541eddf09
Teng Li Qian <tengliqian@202.156.34-241.unknown.starhub.net.sg>
2023-12-18 17:50:31 +0800

    working pgcd

---
+++ 04_exam_rank_02/level_03/pgcd/pgcd.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pgcd.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lteng <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/18 17:36:36 by lteng             #+#    #+#             */
+/*   Updated: 2023/12/18 17:50:05 by lteng            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+
+void	ft_putnbr(int x)
+{
+	char	a;
+
+	a = '0';
+	while (x > 9)
+	{
+		ft_putnbr(x / 10);
+		ft_putnbr(x % 10);
+	}
+	if (x <= 9)
+	{
+		a += x;
+		write(1, &a, 1);
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	int	common_denom;
+
+	common_denom = 1;
+	if (argc == 3 && atoi(argv[1]) > 0 && atoi(argv[2]) > 0)
+	{
+		if (atoi(argv[1]) > atoi(argv[2]))
+			common_denom = atoi(argv[2]);
+		else
+			common_denom = atoi(argv[1]);
+		while (common_denom > 0)
+		{
+			if (atoi(argv[1]) % common_denom == 0
+				&& atoi(argv[2]) % common_denom == 0)
+			{
+				ft_putnbr(common_denom);
+				break ;
+			}
+			common_denom -= 1;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
