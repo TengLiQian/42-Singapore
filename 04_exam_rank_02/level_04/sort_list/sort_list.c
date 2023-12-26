1442af33554c71e0288d16ad47ff707055e70728
Li Qian Teng <lteng@c2r6s10.42singapore.sg>
2023-12-26 19:26:37 +0800

    Not sure if correct

---
+++ 04_exam_rank_02/level_04/sort_list/sort_list.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_list.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lteng <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/26 17:59:51 by lteng             #+#    #+#             */
+/*   Updated: 2023/12/26 19:23:33 by lteng            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+t_list	*sort_list(t_list* lst, int (*cmp)(int, int))
+{
+	int		swap;
+	t_list	*temp;
+
+	temp = lst;
+	while (lst->next != 0)
+	{
+		if (((*cmp)(lst->data, lst->next->data)) == 0)
+		{
+			swap = lst->data;
+			lst->data = lst->next->data;
+			lst->next->data = swap;
+			lst = temp;
+		}
+		else
+			lst = lst->next;
+	}
+	lst = temp;
+	return (lst);
+}
