03497326493f038a3eba93aa28bcca86bc683f49
Li Qian Teng <lteng@c2r6s10.42singapore.sg>
2023-12-26 13:40:23 +0800

    Refer from Github

---
+++ 04_exam_rank_02/level_04/ft_list_remove_if/ft_list_remove_if.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_remove_if.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lteng <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/26 12:09:48 by lteng             #+#    #+#             */
+/*   Updated: 2023/12/26 13:40:00 by lteng            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
+{
+	t_list	*current;
+
+	current = *begin_list;
+	if (begin_list == NULL || *begin_list == NULL)
+		return ;
+	if (cmp(current->data, data_ref) == 0)
+	{
+		*begin_list = current->next;
+		free(current);
+		ft_list_remove_if(begin_list, data_ref, cmp);
+	}
+	current = *begin_list;
+	ft_list_remove_if(&current->next, data_ref, cmp);
+}
