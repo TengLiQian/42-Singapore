f96685f7bd253348c3b6fd2ee84ee718789d8372
Li Qian Teng <lteng@c2r6s10.42singapore.sg>
2023-12-26 11:47:25 +0800

    Found in libft

---
+++ 04_exam_rank_02/level_04/ft_itoa/ft_itoa.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lteng <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/26 11:18:48 by lteng             #+#    #+#             */
+/*   Updated: 2023/12/26 11:47:01 by lteng            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int	ft_count(long n)
+{
+	int	i;
+
+	i = 0;
+	if (n == 0)
+		return (1);
+	if (n < 0)
+	{
+		n *= -1;
+		i++;
+	}
+	while (n != 0)
+	{
+		n /= 10;
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_itoa(int n)
+{
+	int			len;
+	long		temp;
+	char		*result;
+
+	temp = n;
+	len = ft_count(n);
+	result = (char *)malloc(sizeof(char) * (len + 1));
+	if (!result)
+		return (NULL);
+	result[len] = '\0';
+	if (n < 0)
+		temp *= -1;
+	while (len > 0)
+	{
+		result[len - 1] = temp % 10 + '0';
+		temp /= 10;
+		len--;
+	}
+	if (n < 0)
+		result[0] = '-';
+	return (result);
+}
+
+int	main(void)
+{
+	char	*result;
+
+	result = ft_itoa(-12324242);
+	printf("%s", result);
+	free(result);
+	return (0);
+}
