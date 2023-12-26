749cb0b2a84527826222244ae313b33be956f4b2
Li Qian Teng <lteng@c2r6s10.42singapore.sg>
2023-12-26 16:40:57 +0800

    Code working, but split has more than 25 lines. Thus, declared multiple variables on one line

---
+++ 04_exam_rank_02/level_04/ft_split/ft_split.c
@@ -0,0 +1,96 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lteng <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/26 13:42:19 by lteng             #+#    #+#             */
+/*   Updated: 2023/12/26 16:40:24 by lteng            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int	word_count(char *str)
+{
+	int	i;
+	int	words;
+
+	words = 0;
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '\0')
+			return (0);
+		else if (str[i] != ' ' && str[i] != '\t' && str[i] != '\n'
+			&& (str[i] == '\0' || str[i] == ' ' || str[i] == '\t'
+				|| str[i] == '\n'))
+			words++;
+		i++;
+	}
+	return (words);
+}
+
+int	char_count(char *str)
+{
+	int	chars;
+	int	words;
+	int	i;
+
+	chars = 0;
+	words = 0;
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] != ' ' && str[i] != '\t' && str[i] != '\n')
+			chars++;
+		i++;
+	}
+	return (chars);
+}
+
+char	**ft_split(char *str)
+{
+	char	**tab;
+	int		i, j, k;
+
+	i = 0;
+	j = 0;
+	tab = malloc(sizeof(char *) * (word_count(str)) + 1);
+	if (!tab)
+		return (NULL);
+	while (str[i])
+	{
+		k = 0;
+		while (str[i] && (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'))
+			i++;
+		if (str[i])
+		{
+			tab[j] = malloc(sizeof(char) * char_count(str + i) + 1);
+			if (!tab[j])
+				return (NULL);
+			while (str[i] && str[i] != ' ' && str[i] != '\t' && str[i] != '\n')
+				tab[j][k++] = str[i++];
+			tab[j++][k] = '\0';
+		}
+	}
+	tab[j] = NULL;
+	return (tab);
+}
+/*
+int	main(void)
+{
+	char	**result;
+	int		i;
+
+	result = ft_split("Apple Orange Banana");
+	i = 0;
+	while (result[i])
+	{
+		printf("%s\n", result[i]);
+		i++;
+	}
+	return (0);
+}*/
