d3ea49b27fe29e918baa045380fd9a17d639fa9f
Li Qian Teng <lteng@c2r6s10.42singapore.sg>
2023-12-26 12:08:28 +0800

    Completed, not sure how to test

---
+++ 04_exam_rank_02/level_04/ft_list_foreach/ft_list_foreach.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_foreach.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lteng <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/26 11:50:55 by lteng             #+#    #+#             */
+/*   Updated: 2023/12/26 12:05:20 by lteng            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+void	ft_list_foreach(t_list *begin_list, void (*f)(void *))
+{
+	t_list	*list_ptr;
+
+	list_ptr = begin_list;
+	while (list_ptr != NULL)
+	{
+		(*f)(list_ptr->data);
+		list_ptr = list_ptr->next;
+	}
+}
