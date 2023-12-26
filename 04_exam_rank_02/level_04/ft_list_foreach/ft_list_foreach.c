f72b741fc27092830ed8ea79a7007d4311d1b5e0
Li Qian Teng <lteng@c2r6s10.42singapore.sg>
2023-12-26 12:15:19 +0800

    Added test case

--- 04_exam_rank_02/level_04/ft_list_foreach/ft_list_foreach.c
+++ 04_exam_rank_02/level_04/ft_list_foreach/ft_list_foreach.c
@@ -6,7 +6,7 @@
 /*   By: lteng <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/26 11:50:55 by lteng             #+#    #+#             */
-/*   Updated: 2023/12/26 12:09:11 by lteng            ###   ########.fr       */
+/*   Updated: 2023/12/26 12:14:58 by lteng            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,41 @@ void	ft_list_foreach(t_list *begin_list, void (*f)(void *))
 	t_list	*list_ptr;
 
 	list_ptr = begin_list;
-	while (list_ptr != NULL)
+	while (list_ptr)
 	{
 		(*f)(list_ptr->data);
 		list_ptr = list_ptr->next;
 	}
 }
+
+/*
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_data(void *data)
+{
+    printf("%s\n", (char *)data);
+}
+
+int main()
+{
+    // Example linked list creation
+    t_list *node1 = malloc(sizeof(t_list));
+    t_list *node2 = malloc(sizeof(t_list));
+
+    node1->data = "Hello";
+    node1->next = node2;
+
+    node2->data = "World";
+    node2->next = NULL;
+
+    // Applying the function to each element of the linked list
+    ft_list_foreach(node1, print_data);
+
+    // Remember to free allocated memory
+    free(node1);
+    free(node2);
+
+    return 0;
+}
+*/
