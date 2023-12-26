db972d595a4a232f637c3b90b71354e0d297fb86
Li Qian Teng <lteng@c2r6s10.42singapore.sg>
2023-12-26 11:17:39 +0800

    Solved with prime numbers

--- 04_exam_rank_02/level_04/fprime/fprime.c
+++ 04_exam_rank_02/level_04/fprime/fprime.c
@@ -6,12 +6,35 @@
 /*   By: lteng <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/25 13:13:32 by lteng             #+#    #+#             */
-/*   Updated: 2023/12/25 15:05:09 by lteng            ###   ########.fr       */
+/*   Updated: 2023/12/26 11:17:21 by lteng            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <stdio.h>
+
+int	ft_isprime(int nbr)
+{
+	int	i;
+
+	i = 2;
+	if (nbr <= 1)
+		return (0);
+	while (i <= (nbr / 2))
+	{
+		if ((nbr % i) == 0)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+void	ft_print(int i, int nbr)
+{
+	printf("%i", i);
+	if (nbr != i)
+		printf("*");
+}
 
 int	main(int argc, char *argv[])
 {
@@ -26,12 +49,11 @@ int	main(int argc, char *argv[])
 			printf("%i", nbr);
 		while (nbr >= i)
 		{
-			if (nbr % i == 0)
+			if (nbr % i == 0 && ft_isprime(i) == 1)
 			{
-				printf("%i", i);
-				if (nbr != i)
-					printf("*");
-				nbr /= i;
+				ft_print(i, nbr);
+				while ((nbr /= i) % i == 0)
+					ft_print(i, nbr);
 			}
 			i++;
 		}
